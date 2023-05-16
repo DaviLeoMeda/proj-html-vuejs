c26469288d1b0749b5af0b38048e32f73adf916e
DaviLeoMeda <davileo.meda@gmail.com>
2023-05-16 17:49:20 +0200

    Final

--- src/components/FooterFirst.vue
+++ src/components/FooterFirst.vue
@@ -10,8 +10,8 @@ export default {
 
 
 <template>
-    <div class="row">
-        <div class="text-white col-2">
+    <div class="d-flex justify-content-between">
+        <div class="text-white w-25">
             <h5 class="text-uppercase">
                 about
             </h5>
@@ -19,7 +19,7 @@ export default {
                 veritatis ut fugiat asperiores ipsum temporibus quam distinctio alias quisquam labore! Eligendi
                 necessitatibus ullam eveniet.</p>
         </div>
-        <div class="text-white col-2">
+        <div class="text-white ">
             <h5 class="text-uppercase">
                 contact
             </h5>
@@ -30,39 +30,39 @@ export default {
             </p>
             <span><a href="#">info@masterstudy.com</a></span>
         </div>
-        <div class="text-white col-1 gx-1">
+        <div class="text-white" style="width: 150px;">
             <h5 class="text-uppercase">
                 social network
             </h5>
             <div class="row">
-                <span class="social col-3 bg-dark rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-dark rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-facebook"></i></a>
                 </span>
-                <span class="social col-3 bg-pinker rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-pinker rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-instagram"></i></a>
                 </span>
-                <span class="social col-3 bg-petrol rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-petrol rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-linkedin"></i></a>
                 </span>
-                <span class="social col-3 bg-danger rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-danger rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-pinterest-p"></i></a>
                 </span>
-                <span class="social col-3 bg-primary rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-primary rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-twitter"></i></a>
                 </span>
-                <span class="social col-3 bg-pinker rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-discord"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-pinker rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-discord"></i></a>
                 </span>
-                <span class="social col-3 bg-coral rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-google-plus"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-coral rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-google-plus"></i></a>
                 </span>
-                <span class="social col-3 bg-primary rounded-5 p-2">
-                    <a href="#"><i class="fa-brands fa-skype"></i></a>
+                <span class="social col-3 d-flex justify-content-center bg-primary rounded-5 p-2">
+                    <a class="rounded-5" href="#"><i class="fa-brands fa-skype"></i></a>
                 </span>
 
             </div>
         </div>
-        <div class="col-2 p-4">
+        <div class="p-4 w-25">
             <img class="w-100" src="../../public/img/Top-udemy-courses-90-percent-off.jpg" alt="">
         </div>
     </div>
